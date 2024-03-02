f00c2f44f5e66dd01c57dca4b0c417d3d6cda51f
abhay sakariya <abhay.work233@gmail.com>
2024-03-02 11:53:27 +0530

    change in config and model

--- model/post.js
+++ model/post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Import Dependencies
 const mongoose = require("mongoose");
 
@@ -34,37 +33,4 @@ const postSchema = new mongoose.Schema({
 // Model
 const Post = mongoose.model("Post",postSchema);
 
-=======
-// Import Dependencies
-const mongoose = require("mongoose");
-
-// Post Schema
-
-const postSchema = new mongoose.Schema({
-    username:{
-        type : String,
-        default : "abhay"
-    },
-    title:{
-        type : String,
-        required : true
-    },
-    author:{
-        type : String,
-        required : true
-    },
-    desc:{
-        type : String,
-        required : true
-    },
-    date:{
-        type : Date,
-        default : Date.now
-    }
-});
-
-// Model
-const Post = mongoose.model("Post",postSchema);
-
->>>>>>> a53d9f9b55b9d458ac4dc5ef275e7fa810374a4b
 module.exports = Post;
