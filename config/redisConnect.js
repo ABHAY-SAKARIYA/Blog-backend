6b4b59f94b67d5a84d73fac24102b6bfbe826799
abhay sakariya <abhay.work233@gmail.com>
2024-03-02 11:51:53 +0530

    Redis Configauration

--- config/redisConnect.js
+++ config/redisConnect.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Import Dependencies
 const redis = require("redis");
 
@@ -23,29 +22,3 @@ const connectToRedis = () => {
 
 module.exports = connectToRedis;
 
-=======
-// Import Dependencies
-const redis = require("redis");
-
-
-// Redis client variable
-const connectToRedis = () => {
-    const redisClient = redis.createClient({
-        password: process.env.REDIS_PASS,
-        socket: {
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT
-        }
-    });
-    // Connect and Check Connection 
-    redisClient.connect();
-    redisClient.on("connect", () => {
-        console.log("connected to redis");
-    });
-
-    return redisClient;
-}
-
-module.exports = connectToRedis;
-
->>>>>>> a53d9f9b55b9d458ac4dc5ef275e7fa810374a4b
